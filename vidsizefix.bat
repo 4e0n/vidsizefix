ab0c0e5bc5a61ea07f3f1ddeaefaaed59f28ac1f
Barkın İlhan <barkin@unrlabs.org>
2020-03-26 21:48:49 +0300

    Update vidsizefix.bat
    Final retouches

--- vidsizefix.bat
+++ vidsizefix.bat
@@ -1,15 +1,41 @@
-rem NEU Meram Tip Fakultesi MP4 dosyasi boyut ayarlayici
-rem GPL v3 - Barkin Ilhan (barkin@unrlabs.org)
+rem vidsizefix - Video file size limiter ffmpeg wrapper DOS batch script
+rem   Copyright (C) 2020 Barkin Ilhan
+   
+rem  This program is free software: you can redistribute it and/or modify
+rem  it under the terms of the GNU General Public License as published by
+rem  the Free Software Foundation, either version 3 of the License, or
+rem  (at your option) any later version.
+rem  This program is distributed in the hope that it will be useful,
+rem  but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem  GNU General Public License for more details.
+rem  You should have received a copy of the GNU General Public License
+rem  along with this program.  If no:t, see <https://www.gnu.org/licenses/>.
+
+rem  Contact info:
+rem  E-Mail:  barkin@unrlabs.org
+rem  Website: http://icon.unrlabs.org/staff/barkin/
+rem  Repo:    https://github.com/4e0n/
+
+rem This script has been written due to the emerged need of limiting
+rem the size of .MP4 files recorded by university professors
+rem for remote education in Windows environment. These video files
+rem are around 20-30 minutes in duration, yet depending on the content
+rem being vivid or stable the sizes vary between 50 to 300mb. In that
+rem script, regardless of their initial size, they are shrinked to
+rem 40mb with H264 proper loss, over ffmpeg.
 
 @echo off
 
-rem --- Hedef dosya buyuklugu -> 40mb
+rem --- Destination file size by default -> 40mb (change if needed)
 set /a size=40
 
+rem --- Audio bitrate
 set /a abitrate=128
+
 set ifn=%1
 
-rem --- Hedef dosya adi
+rem --- dstfilename.mp4=srcfilename-XXmb.mp4
 for /f "tokens=1,2 delims=." %%a in ("%ifn%") do (
  set froot=%%a
  set fext=%%b
